a13707a2ea6658cfb53f2296e3b5d98443bcaa1d
Toberiuz <haatainentopi@gmail.com>
2024-09-21 19:33:37 +0300

    pohja josta lähteä kasaamaan server ja client meidän projektiin sopivaksi, server osaa ottaa useamman clientin eri threadeille samanaikaisesti ja osaa timeouttaa threadit yks kerrallaan. client timeout ei vielä toimi

---
+++ src/main/java/Client.java
@@ -0,0 +1,73 @@
+// A simple Client Server Protocol .. Client for Echo Server
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class Client {
+
+public static void main(String args[]) throws IOException{
+
+
+    InetAddress address=InetAddress.getLocalHost();
+    Socket s1=null;
+    String line=null;
+    BufferedReader br=null;
+    BufferedReader is=null;
+    PrintWriter os=null;
+
+    try {
+        s1=new Socket(address, 4445); // You can use static final constant PORT_NUM
+        s1.setSoTimeout(1000);
+        br= new BufferedReader(new InputStreamReader(System.in));
+        is=new BufferedReader(new InputStreamReader(s1.getInputStream()));
+        os= new PrintWriter(s1.getOutputStream());
+    }
+    catch (IOException e){
+        e.printStackTrace();
+        System.err.print("IO Exception");
+    }
+
+    System.out.println("Client Address : "+address);
+    System.out.println("Enter Data to echo Server ( Enter QUIT to end):");
+
+    String response=null;
+    try{
+        line=br.readLine(); 
+        while(true){
+                os.println(line);
+                os.flush();
+                try{
+                response=is.readLine();
+                if (response != null){
+                System.out.println("Server Response : "+response);
+                }
+            } catch (SocketTimeoutException e) {
+                System.out.println("no message from server Timeout");
+            }
+                line=br.readLine();
+                if (line.compareTo("QUIT")==0){
+                    break;
+                }
+            }
+
+
+
+    }
+    catch(IOException e){
+        e.printStackTrace();
+    System.out.println("Socket read Error");
+    }
+    finally{
+
+        is.close();os.close();br.close();s1.close();
+                System.out.println("Connection Closed");
+
+    }
+
+}
+}
