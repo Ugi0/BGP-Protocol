11436b55a26bacc3ef86867034ce991d4e068a6c
Amy Nymalm <amy.a.nymalm@utu.fi>
2024-09-27 13:26:12 +0300

    Header creation,  might have to be moved elsewhere.

--- src/main/data/Message.java
+++ src/main/data/Message.java
@@ -3,6 +3,9 @@ package main.data;
 public class Message {
     protected static final int HEADER_SIZE = 19;
     private static final int HEADER_MARKER_SIZE = 16;
+    private static final int HEADER_LENGTH_SIZE = 2;
+    private static final int HEADER_TYPE_SIZE = 1;
+    private static final int MAX_MESSAGE_LENGTH = 65535;
 
     private int[] message;
     protected int type;
@@ -17,8 +20,8 @@ public class Message {
                 throw new Error();
             }
         }
-        length = getValue(HEADER_MARKER_SIZE+1, 2);
-        type = message[HEADER_MARKER_SIZE+3];
+        length = getValue(HEADER_MARKER_SIZE, 2);
+        type = message[HEADER_MARKER_SIZE+2];
     }
 
     protected int getValue(int startIndex, int octects) {
@@ -29,4 +32,28 @@ public class Message {
         }
         return res;
     }
+
+    public int[] createHeader(int length, int type){
+        
+        if (length > MAX_MESSAGE_LENGTH || length < 0) {
+            throw new Error("invalid message length");
+        }
+        if (type > 5 || type < 1) {
+            throw new Error("invalid type");
+        }
+        
+        //Marker creation
+        int[] header =new int[HEADER_SIZE];
+        for (int i = 0; i < HEADER_MARKER_SIZE; i++) {
+            header[i] = 255;
+        }
+        // Length creation
+        for (int i = HEADER_MARKER_SIZE; i < HEADER_MARKER_SIZE + HEADER_LENGTH_SIZE; i++) {
+            header[i] = (length >> (8 * (HEADER_LENGTH_SIZE - 1 - i))) & 0xFF;
+        }
+        //Type creation
+        header[HEADER_MARKER_SIZE+2] = type;
+
+        return header;
+    };
 }
